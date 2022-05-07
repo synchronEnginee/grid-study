89bcaa7a06ac93a74d60207c770858cb754c5cba
synchronEnginee <te2.9niku.41cd@gmail.com>
2022-05-07 12:07:02 +0900

    initial calculate

--- js/main.js
+++ js/main.js
@@ -9,3 +9,24 @@ open.on('click', function () {
 close.on('click', function () {
   modal.removeClass("open");
 });
+
+document.getElementById('btn').addEventListener('click', ()=>{
+  const age = document.getElementsByName('age');
+  let ageLength = age.length
+  let checkedAge = '';
+  for (let i = 0; i < ageLength; i++) {
+    if (age.item(i).checked) {
+        checkedAge = age.item(i).value;
+    }
+  }
+  // 冗長なので後で関数切り出す
+  const size = document.getElementsByName('size');
+  const sizeLength = size.length
+  let checkedSize = '';
+  for (let i = 0; i < sizeLength; i++) {
+    if (age.item(i).checked) {
+        checkedSize = size.item(i).value;
+    }
+  }
+  console.log(checkedAge, checkedSize);
+})
